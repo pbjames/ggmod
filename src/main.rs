71317f5e645e7c8fc51b02a3ee089f80a1159b5f
James <jameskowal10@gmail.com>
2024-12-16 11:05:33 +0000

    Use clap example

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,45 @@
-fn main() {}
+use clap::{Parser, Subcommand};
+use std::path::PathBuf;
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    /// Optional name to operate on
+    name: Option<String>,
+
+    /// Sets a custom config file
+    #[arg(short, long, value_name = "FILE")]
+    config: Option<PathBuf>,
+
+    /// Provide more debugging information
+    #[arg(short, long, action = clap::ArgAction::Count)]
+    verbose: u8,
+
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Download mod from GB
+    Download {
+        /// Also install or no
+        #[arg(short, long)]
+        install: bool,
+    },
+    /// dragon install
+    Install {},
+
+    /// Uninstall mod - can be installed again later
+    Uninstall {},
+}
+
+fn main() {
+    let cli = Cli::parse();
+    match &cli.command {
+        Some(Commands::Download { install: list }) => {}
+        Some(Commands::Install {}) => {}
+        Some(Commands::Uninstall {}) => {}
+        None => {}
+    }
+}
