64d1d5d98696df92026a9e6a7f48c1cfd377fffe
James K. <jameskowal10@gmail.com>
2024-12-16 11:35:29 +0000

    Refactor path code + add test

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,41 @@
 use directories::BaseDirs;
 use http::{Request, Response};
-use std::fs::{self, DirBuilder, File};
+use std::{fs, io};
 
 const SUBDIR_NAME: &str = "ggmod";
 
-fn cache_path() -> Option<std::path::PathBuf> {
-    Some(BaseDirs::new()?.config_dir().join(SUBDIR_NAME))
+fn download_path() -> Option<std::path::PathBuf> {
+    Some(
+        BaseDirs::new()?
+            .cache_dir()
+            .join(SUBDIR_NAME)
+            .join("downloads"),
+    )
 }
 
-fn check_cache_path() -> Result<(), std::io::Error> {
-    // TODO: Erase unwrap call
-    DirBuilder::new()
-        .recursive(true)
-        .create(cache_path().unwrap())
+pub fn check_download_path() -> Result<(), std::io::Error> {
+    if let Some(path) = download_path() {
+        fs::DirBuilder::new().recursive(true).create(path)
+    } else {
+        Err(io::Error::new(
+            io::ErrorKind::NotFound,
+            "download path not foudn",
+        ))
+    }
 }
 
 //fn download_mod(mod_id: usize) -> Result<std::path::PathBuf, &str> {}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn check_download_path_creates() {
+        // TODO: This looks dumb fix it
+        let path = download_path().unwrap();
+        fs::remove_dir(path.clone()).unwrap_or(());
+        check_download_path().unwrap();
+        fs::read_dir(path).unwrap();
+    }
+}
