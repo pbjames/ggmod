a32b35a2c2e1b2f4c0a7d57ed189b14a590e7709
James K. <jameskowal10@gmail.com>
2024-12-18 15:26:07 +0000

    Add mod cache + downloading

--- src/files.rs
+++ src/files.rs
@@ -1,8 +1,15 @@
 use directories::BaseDirs;
 use log::info;
-use std::{fs, io};
+use serde::Serialize;
+use std::{
+    fs,
+    io::{self, Read, Write},
+};
+
+use crate::gamebanana::Mod;
 
 pub const SUBDIR_NAME: &str = "ggmod";
+pub const REGISTRY_FN: &str = "registry.json";
 
 pub fn download_path() -> Option<std::path::PathBuf> {
     Some(
@@ -13,11 +20,38 @@ pub fn download_path() -> Option<std::path::PathBuf> {
     )
 }
 
-pub fn check_download_path() -> Result<(), std::io::Error> {
+pub fn check_download_path() -> Result<std::path::PathBuf, io::Error> {
     if let Some(path) = download_path() {
-        info!("Download path not found, creating");
-        fs::DirBuilder::new().recursive(true).create(path)
+        fs::DirBuilder::new().recursive(true).create(&path)?;
+        Ok(path)
     } else {
         Err(io::Error::new(io::ErrorKind::NotFound, "Path inaccessible"))
     }
+}
+
+pub fn check_registry() -> Result<std::path::PathBuf, io::Error> {
+    check_download_path()?;
+    let reg_path = check_download_path()?.join(REGISTRY_FN);
+    if !reg_path.is_file() {
+        info!("Write new registry.json");
+        fs::File::create(&reg_path)?;
+    }
+    Ok(reg_path)
+}
+
+pub fn registry_has_id(mod_id: usize) -> Result<Option<Mod>, io::Error> {
+    // TODO: Use serde_json::from_reader and writer
+    let path = check_registry()?;
+    let mut file = fs::OpenOptions::new().read(true).open(path)?;
+    let mut s_ptr = String::new();
+    file.read_to_string(&mut s_ptr)?;
+    let obj: Vec<Mod> = serde_json::from_str(&s_ptr)?;
+    Ok(obj.iter().find(|m| m.id == mod_id).cloned())
+}
+
+pub fn register_object<T: Serialize>(obj: T) -> Result<(), io::Error> {
+    // TODO: Use serde_json::from_reader and writer
+    let path = check_registry()?;
+    let mut file = fs::OpenOptions::new().append(true).open(path)?;
+    writeln!(file, "{:?}", serde_json::to_string_pretty(&obj))
 }
--- src/gamebanana.rs
+++ src/gamebanana.rs
@@ -1,5 +1,15 @@
+use std::{
+    fs,
+    io::{self, Cursor},
+    path,
+};
+
 use reqwest::blocking;
 use serde::{Deserialize, Serialize};
+
+use crate::{download_path, register_object, registry_has_id};
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 
 // TODO: Refactor structs and json format so its less ugly
 // inconsistent endpoint alternative:
@@ -23,23 +33,77 @@ pub struct GBFile {
     _sDescription: String,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-pub struct GBMod {
-    _aCategory: GBCategory,
-    _aFiles: Vec<GBFile>,
-    _sName: String,
-    _sDescription: String,
+impl GBFile {
+    pub fn download_to(&self, path: path::PathBuf) -> Result<path::PathBuf> {
+        let response = blocking::get(&self._sDownloadUrl)?;
+        let mut file = fs::File::create(&path)?;
+        let mut content = Cursor::new(response.bytes()?);
+        io::copy(&mut content, &mut file)?;
+        Ok(path)
+    }
+
+    pub fn fetch(&self) -> Result<path::PathBuf> {
+        let path = download_path().unwrap_or_default().join(&self._sFile);
+        if path.exists() && path.is_file() {
+            Ok(path)
+        } else {
+            self.download_to(path)
+        }
+    }
 }
 
-pub fn download(mod_id: usize) -> Option<GBMod> {
-    // TODO: This needs to return more context if it dies
-    let uri = format!(
-        "https://gamebanana.com/apiv6/Mod/{}?\
+#[derive(Serialize, Deserialize, Debug)]
+pub struct GBMod {
+    pub _aCategory: GBCategory,
+    pub _aFiles: Vec<GBFile>,
+    pub _sName: String,
+    pub _sDescription: String,
+}
+
+impl GBMod {
+    pub fn files(&self) -> &Vec<GBFile> {
+        &self._aFiles
+    }
+    pub fn download_file(&self, idx: usize) -> Result<path::PathBuf> {
+        self._aFiles[idx].fetch()
+    }
+
+    pub fn build(id: usize) -> Result<GBMod> {
+        let uri = format!(
+            "https://gamebanana.com/apiv6/Mod/{}?\
         _csvProperties=_sName,_aGame,_sProfileUrl,_aPreviewMedia,\
         _sDescription,_aSubmitter,_aCategory,_aSuperCategory,_aFiles,\
         _tsDateUpdated,_aAlternateFileSources,_bHasUpdates,_aLatestUpdates",
-        mod_id
-    );
-    let resp = blocking::get(uri).ok()?.text().ok()?;
-    serde_json::from_str(&resp).ok()?
+            id
+        );
+        let resp = blocking::get(uri)?.text()?;
+        Ok(serde_json::from_str::<GBMod>(&resp)?)
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Mod {
+    pub id: usize,
+    character: String,
+    path: path::PathBuf,
+    name: String,
+    description: String,
+}
+
+impl Mod {
+    pub fn get(id: usize) -> Option<Mod> {
+        registry_has_id(id).ok()?
+    }
+
+    pub fn build(id: usize, gbmod: GBMod, idx: usize) -> Result<Mod> {
+        let m = Mod {
+            id,
+            character: gbmod._aCategory._sName.clone(),
+            path: gbmod.download_file(idx)?,
+            name: gbmod._sName,
+            description: gbmod._sDescription,
+        };
+        register_object(&m)?;
+        Ok(m)
+    }
 }
