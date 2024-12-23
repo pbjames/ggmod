f874b18302fc82b0774789477c29a9fbcbba13f3
James <jameskowal10@gmail.com>
2024-12-23 09:38:06 +0000

    QoL changes

--- src/gamebanana.rs
+++ src/gamebanana.rs
@@ -1,12 +1,7 @@
-use std::{
-    fs,
-    io::{self, Cursor},
-    path,
-};
+use std::{fs, io, path};
 
 use compress_tools::{uncompress_archive, Ownership};
-use log::{debug, info};
-use reqwest::blocking;
+use log::{debug, info, trace};
 use serde::{Deserialize, Serialize};
 
 use crate::download_path;
@@ -45,9 +40,9 @@ pub struct GBModPage {
 impl GBFile {
     pub fn download_to<'a>(&self, path: &'a path::PathBuf) -> Result<&'a path::PathBuf> {
         info!("Downloading new archive..");
-        let response = blocking::get(&self.download_url)?;
+        let response = reqwest::blocking::get(&self.download_url)?;
         let mut file = fs::File::create(path)?;
-        let mut content = Cursor::new(response.bytes()?);
+        let mut content = io::Cursor::new(response.bytes()?);
         io::copy(&mut content, &mut file)?;
         Ok(path)
     }
@@ -56,6 +51,7 @@ impl GBFile {
         let file = download_path().unwrap_or_default().join(&self.file);
         let dir = file.with_extension("");
         if dir.exists() && dir.is_dir() {
+            trace!("Mod already exists, doing nothing");
             Ok(dir)
         } else {
             self.download_to(&file)?;
@@ -73,34 +69,43 @@ impl GBModPage {
     }
 
     pub fn build(id: usize) -> Result<GBModPage> {
+        let resp = reqwest::blocking::get(Self::fetch_uri(id))?.text()?;
+        let conv = Self::convert_to_snake(&resp);
+        Ok(serde_json::from_str::<GBModPage>(&conv)?)
+    }
+
+    fn fetch_uri(id: usize) -> String {
         // INFO: Just in case they ever fix the key names
-        // https://api.gamebanana.com/Core/Item/Data?itemtype=Mod&itemid={mod_id}&fields=Category().name,creator,date,description,downloads,Files().aFiles(),likes,name,Nsfw().bIsNsfw()&return_keys=true&format=json
-        let uri = format!(
+        // https://api.gamebanana.com/Core/Item/Data?
+        // itemtype=Mod&itemid={mod_id}
+        // &fields=Category().name,creator,date,description,downloads,
+        // Files().aFiles(),likes,name,Nsfw().bIsNsfw()
+        // &return_keys=true&format=json
+        format!(
             "https://gamebanana.com/apiv6/Mod/{id}?\
         _csvProperties=_sName,_aGame,_sProfileUrl,_aPreviewMedia,\
         _sDescription,_aSubmitter,_aCategory,_aSuperCategory,_aFiles,\
         _tsDateUpdated,_aAlternateFileSources,_bHasUpdates,_aLatestUpdates,\
         _idRow",
-        );
-        let resp = blocking::get(uri)?.text()?;
-        Ok(serde_json::from_str::<GBModPage>(
-            &resp
-                .replace("_aCategory", "category")
-                .replace("_aFiles", "files")
-                .replace("_aFile", "file")
-                .replace("_sFiles", "files")
-                .replace("_sFile", "file")
-                .replace("_sName", "name")
-                .replace("_sDescription", "description")
-                .replace("_bContainsExe", "contains_exe")
-                .replace("_nDownloadCount", "download_count")
-                .replace("_nFilesize", "filesize")
-                .replace("_sAnalysisResultCode", "analysis_result_code")
-                .replace("_tsDateAdded", "date_added")
-                .replace("_sMd5Checksum", "md5")
-                .replace("_sDownloadUrl", "download_url")
-                .replace("_sIconUrl", "icon_url")
-                .replace("_idRow", "id"),
-        )?)
+        )
+    }
+
+    fn convert_to_snake(s: &str) -> String {
+        s.replace("_aCategory", "category")
+            .replace("_aFiles", "files")
+            .replace("_sFiles", "files")
+            .replace("_aFile", "file")
+            .replace("_sFile", "file")
+            .replace("_sName", "name")
+            .replace("_sDescription", "description")
+            .replace("_bContainsExe", "contains_exe")
+            .replace("_nDownloadCount", "download_count")
+            .replace("_nFilesize", "filesize")
+            .replace("_sAnalysisResultCode", "analysis_result_code")
+            .replace("_tsDateAdded", "date_added")
+            .replace("_sMd5Checksum", "md5")
+            .replace("_sDownloadUrl", "download_url")
+            .replace("_sIconUrl", "icon_url")
+            .replace("_idRow", "id")
     }
 }
