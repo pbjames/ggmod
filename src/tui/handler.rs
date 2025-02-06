30845f34ca09ed6dc5e1a22d8032dbd4e0b77421
James K. <jameskowal10@gmail.com>
2025-02-06 14:28:52 +0000

    Add seperate event loop

--- src/tui/handler.rs
+++ src/tui/handler.rs
@@ -16,24 +16,32 @@ use super::{
     ui::show_ui,
 };
 
-type Amt<B> = Arc<Mutex<Terminal<B>>>;
+type Am<T> = Arc<Mutex<T>>;
 
 pub async fn run_tui(collection: LocalCollection) {
     let terminal = Arc::new(Mutex::new(ratatui::init()));
-    let mut app = App::new(collection).await;
-    let (term, rx_terminate) = Termination::new();
-    tokio::spawn(async move {
-        draw_loop(terminal, &mut app, term.clone(), rx_terminate.resubscribe()).await
-    })
-    .await
-    .unwrap();
-    // theres nothing else running for now
+    let app = Arc::new(Mutex::new(App::new(collection).await));
+    let app_copy = app.clone();
+    let (termination, rx_terminate) = Termination::new();
+    let rx_terminate_copy = rx_terminate.resubscribe();
+    tokio::spawn(async move { draw_loop(terminal, app_copy, rx_terminate).await });
+    event_loop(app, termination, rx_terminate_copy).await;
+}
+
+async fn event_loop(app: Arc<Mutex<App>>, term: Termination, mut rx_term: Receiver<usize>) {
+    loop {
+        if rx_term.try_recv().unwrap_or(0) == 1 {
+            ratatui::restore();
+            break;
+        }
+        let mut appref = app.lock().await;
+        handle_event(&mut appref, &term).await
+    }
 }
 
 async fn draw_loop<B: Backend>(
-    terminal: Amt<B>,
-    app: &mut App,
-    term: Termination,
+    terminal: Am<Terminal<B>>,
+    app: Am<App>,
     mut rx_term: Receiver<usize>,
 ) {
     loop {
@@ -41,14 +49,14 @@ async fn draw_loop<B: Backend>(
             ratatui::restore();
             break;
         }
+        let mut appref = app.lock().await;
         terminal
             .lock()
             .await
             .draw(|f| {
-                show_ui(f, app);
+                show_ui(f, &mut appref);
             })
             .unwrap();
-        handle_event(app, &term).await;
     }
 }
 
